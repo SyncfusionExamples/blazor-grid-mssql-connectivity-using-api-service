b8ffcce13c5e948ec734c4e41ce9f8346f01ac2c
vignesh.natarajan@syncfusion.com <Nssdv@1994>
2024-02-27 15:13:03 +0530

    sample added

---
+++ GridMSSql/BlazorApp6/Program.cs
@@ -0,0 +1,33 @@
+using BlazorApp6.Components;
+using Syncfusion.Blazor;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped<HttpClient>(factory => new HttpClient(new HttpClientHandler()
+{
+    ServerCertificateCustomValidationCallback = (HttpRequestMessage m, System.Security.Cryptography.X509Certificates.X509Certificate2 f, System.Security.Cryptography.X509Certificates.X509Chain x, System.Net.Security.SslPolicyErrors d) => true
+}));
+builder.Services.AddSyncfusionBlazor();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
